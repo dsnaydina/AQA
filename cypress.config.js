0054fcdc8d652013b59da6c134b9c6b6c5d8ca47
WinWare <you@example.com>
2023-02-22 14:44:20 +0100

    added test for goal based on click event and url

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "b1fgmp",
   e2e: {
-    //baseUrl: "https://app.staging.winware.ai/",
+    baseUrl: "https://app.staging.winware.ai",
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
